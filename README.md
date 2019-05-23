fa6399941ed7fcd39904587ad510403e00835440
Casey Graff <caseyagraff@gmail.com>
2019-05-23 15:09:06 -0700

    Updated Readme and added basic click commands to main

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ CS 295 Deep Generative Models project on GAN-based super-resolution of NDVI.
 conda install -c conda-forge pyhdf
 conda install -c pytorch pytorch
 conda install matplotlib
+conda install click
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # Entrance for runnning 
+import click
+
+from src.data import modis_download
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def download():
+    modis_download.modis_download()
+
+@click.command()
+def train():
+    raise NotImplementedError()
+
+
+cli.add_command(download)
+cli.add_command(train)
+
+if __name__ == '__main__':
+    cli()
