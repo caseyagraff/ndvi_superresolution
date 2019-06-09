7932cc5c73b791d4f16663aed61c117d1abe16b9
Preston James Putzel <pputzel@datalab-6.ics.uci.edu>
2019-06-09 11:42:14 -0700

    added function for iterating through data directory and calling pre-processing coed

--- src/data/extract_patches.py
+++ src/data/extract_patches.py
@@ -1,37 +1,4 @@
 import numpy as np
-import pickle
-import os
-from pyhdf.SD import SD, SDC
-from pyhdf import HDF
-
-'''
-Load cell data from .hdf files. Automatically determines 250m vs 500m resolution.
-Input:
-filename: string
-Returns:
-data: returns as a numpy array of dimensions 4800x4800 for 250m resolution, and 2400x2400 for 500m resolution.
-
-'''
-def load_data_from_files(filename):
-    if not os.path.exists(filename):
-        print("File {} does not exist, cannot load data.".format(filename))
-        return
-    elif not HDF.ishdf(filename):
-        print("File {} is not in hdf4 file format, cannot load data.".format(filename))
-        return
-
-    f = SD(filename, SDC.READ)
-    data_field = None
-    for i, d in enumerate(f.datasets()):
-        # print("{0}. {1}".format(i+1,d))
-        if "NDVI" in d:
-            data_field = d
-
-    ndvi_data = f.select(data_field)
-    print("NDVI dimensions: {}".format(ndvi_data.dimensions()))
-    data = np.array(ndvi_data.get())
-    return data
-
 
 '''
 Takes in a N by N numpy array of a single cell and outputs the patches for that cell
@@ -63,7 +30,6 @@ def extract_patches_single_cell(full_cell, patch_size, date, cell_id):
             metadata_list.append((date, cell_id, patch_xid, patch_yid))
     return np.array(patch_list), np.array(metadata_list)
 
-
 '''
 filters out patches which are mostly water by using the low-res image only.
 '''
@@ -88,9 +54,8 @@ def is_mostly_water(patch, percent_threshold=50., water_threshold=-3000):
     else: 
         return True
 
-
 '''
-makes patches for all cells and filters. Saves a block for each cell in output dir
+makes patches for all cells and filters them. Saves a block for each cell in output dir
 '''
 def make_patches_and_filter_all_cells(low_res_cells, high_res_cells, patch_size, low_res_amount, high_res_amount, output_dir, dates, cell_ids):
 
@@ -106,7 +71,6 @@ def make_patches_and_filter_all_cells(low_res_cells, high_res_cells, patch_size,
         with open(os.path.join(output_dir, savename), 'wb') as f:
             pickle.dump((low_res_patches, high_res_patches, metadata), f)
 
-
 '''
 TODO: make function which aggregates the per cell patch data into larger chunks containing data 
 from multiple cells
@@ -115,4 +79,24 @@ def aggregate_chunks():
     pass    
         
          
+def run_extract_patches(high_res_cell_dir, low_res_cell_dir, patch_size, output_dir, high_low_ratio=5):
+    high_res_cell_years = os.listdir(high_res_cell_dir)
+    low_res_cell_years = os.listdir(low_res_cell_dir)
 
+    high_res_cells = []
+    low_res_cells = []
+    dates = []
+    cell_ids = []
+    for year in high_res_cell_years:
+        for high_res_cell, low_res_cell in zip(os.listdir(high_res_cell_dir, year), os.listdir(low_res_cell_dir, year)):
+            high_res_cells.append(hdf_to_array(os.path.join(high_res_cell_dir, year, high_res_cell)))
+            low_res_cells.append(hdf_to_array(os.path.join(low_res_cell_dir, year, low_res_cell)))
+            dates.append(high_res_cell.split('.')[1][1:]
+            cell_ids.append(high_res_cell.split('.')[2])
+        #now process all the cells per year using make_patches_and_filter_all_cells
+        #write results per year to an output/year
+    
+    #Aggregate chunks if desired 
+
+    
+    
