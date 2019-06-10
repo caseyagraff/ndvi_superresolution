b604e4c5a1273915cc9de1d0388c6d05689cc3e9
Chirag Choudhary <chirag5624@gmail.com>
2019-06-09 17:02:55 -0700

    fixed bugs in model architecture

--- src/models.py
+++ src/models.py
@@ -6,17 +6,17 @@ from src.loss_functions import mse_loss
 class ResidualBlock(torch.nn.Module):
     def __init__(self):
         super(ResidualBlock, self).__init__()
-        self.conv1 = Conv2d(64, 64, 3)
+        self.conv1 = Conv2d(64, 64, 3, padding=1)
         self.bn1 = BatchNorm2d(64)
 
         self.prelu = PReLU()
 
-        self.conv2 = Conv2d(64, 64, 3)
+        self.conv2 = Conv2d(64, 64, 3, padding=1)
         self.bn2 = BatchNorm2d(64)
 
     def forward(self, x):
         identity = x
-        out = self.conv1()
+        out = self.conv1(identity)
         out = self.bn1(out)
         out = self.prelu(out)
 
@@ -29,26 +29,25 @@ class ResidualBlock(torch.nn.Module):
 
 
 class Generator(torch.nn.Module):
-    model = None
+
     def __init__(self, num_blocks=1):
         super(Generator, self).__init__()
-        self.conv1 = Conv2d(1, 64, kernel_size=9)
+        print("Initialized generator network..")
+        self.conv1 = Conv2d(1, 64, kernel_size=9, padding=4)
         self.prelu = PReLU()
 
         self.layers = self.get_residual_blocks(num_blocks)
 
-        self.conv2 = Conv2d(64, 64, kernel_size=3)
+        self.conv2 = Conv2d(64, 64, kernel_size=3, padding=1)
         self.bn2 = BatchNorm2d(64)
 
-        self.conv3 = Conv2d(1, 256, kernel_size=3)
-        self.pxshuffle = PixelShuffle()
-        self.bn3 = BatchNorm2d(64)
+        self.conv3 = Conv2d(64, 256, kernel_size=3, padding=1)
+        self.pxshuffle = PixelShuffle(upscale_factor=2)  # up-sampling
 
-        self.conv4 = Conv2d(1, 256, kernel_size=3)
-        self.bn4 = BatchNorm2d(256)
+        # used in original SR-GAN paper, only for 4x up-sampling
+        # self.conv4 = Conv2d(256, 256, kernel_size=3, padding=1)
 
-        self.conv5 = Conv2d(256, 1, kernel_size=9)
-
+        self.conv5 = Conv2d(64, 1, kernel_size=9, padding=4)
 
     def get_residual_blocks(self, num_blocks):
         layers = []
@@ -62,34 +61,31 @@ class Generator(torch.nn.Module):
         out = self.conv1(x)
         out = self.prelu(out)
 
-        out1 = out
+        identity = out
         out = self.layers(out)
 
         out = self.conv2(out)
         out = self.bn2(out)
-        out += out1
+        out += identity
         out = self.prelu(out)
 
         out = self.conv3(out)
         out = self.pxshuffle(out)
         out = self.prelu(out)
 
-        out = self.conv4(out)
-        out = self.pxshuffle(out)
-        out = self.prelu(out)
-
         res = self.conv5(out)
+        print(res.shape)
         return res
 
 
 class DiscriminatorBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, stride):
+    def __init__(self, in_channels, out_channels, stride, padding):
         super(DiscriminatorBlock, self).__init__()
 
-        self.model = torch.nn.Sequential([
-            Conv2d(in_channels, out_channels, 3, stride),
+        self.model = torch.nn.Sequential(*[
+            Conv2d(in_channels, out_channels, 3, stride, padding),
             BatchNorm2d(out_channels),
-            LeakyReLU(),
+            LeakyReLU()
         ])
 
     def forward(self, x):
@@ -99,19 +95,20 @@ class DiscriminatorBlock(torch.nn.Module):
 class Discriminator(torch.nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
+        print("Initialized discriminator network..")
         self.num_blocks = 3
-        self.model = torch.nn.Sequential([
-            Conv2d(1, 64, 3),
+        self.model = torch.nn.Sequential(*[
+            Conv2d(1, 64, 3, 1),
             LeakyReLU(),
-            DiscriminatorBlock(64, 64, 2),
-            DiscriminatorBlock(64, 128, 1),
-            DiscriminatorBlock(128, 128, 2),
-            DiscriminatorBlock(128, 256, 1),
-            DiscriminatorBlock(256, 256, 2),
-            DiscriminatorBlock(256, 512, 1),
-            DiscriminatorBlock(512, 512, 2),
+            DiscriminatorBlock(64, 64, 2, 1),
+            DiscriminatorBlock(64, 128, 1, 1),
+            DiscriminatorBlock(128, 128, 2, 1),
+            DiscriminatorBlock(128, 256, 1, 1),
+            DiscriminatorBlock(256, 256, 2, 1),
+            DiscriminatorBlock(256, 512, 1, 1),
+            DiscriminatorBlock(512, 512, 2, 1),
             Flatten(),
-            Linear(32*32*512, 1024),
+            Linear(320000, 1024),
             LeakyReLU(),
             Linear(1024, 1),
             Sigmoid()
@@ -125,20 +122,26 @@ class Discriminator(torch.nn.Module):
 
 
 class SuperResolutionGAN(torch.nn.Module):
+    # default parameters used in original paper
     batch_size = None
     num_generator_steps = None
     num_discriminator_steps = None
     num_epochs = 100000
     generator = None
     discriminator = None
+    learning_rate = 0.00001
 
     def __init__(self):
         super(SuperResolutionGAN, self).__init__()
-        self.discriminator = Discriminator()
-        self.generator = Generator()
+        print("Initialized SR-GAN model..")
 
     def forward(self, x):
-        raise NotImplementedError()
+        generator = Generator()
+        sample_high_res = generator.forward(x)
+
+        discriminator = Discriminator()
+        pred = discriminator.forward(sample_high_res)
+        print("Discriminator predictions: {}".format(pred))
 
     def test(self):
         raise NotImplementedError
@@ -150,4 +153,5 @@ class SuperResolutionGAN(torch.nn.Module):
 class Flatten(torch.nn.Module):
     def forward(self, x):
         batch_size = x.shape[0]
+        print("batch size: {}".format(batch_size))
         return x.view(batch_size, -1)
