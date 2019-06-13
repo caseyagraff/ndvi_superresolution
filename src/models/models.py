a7a12c4aa6e2ad8b58d307bf1aa7d13f31d07aca
Chirag Choudhary <chirag5624@gmail.com>
2019-06-12 20:25:41 -0700

    added GAN, MSE loss functions, modified Discriminator class

--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import LeakyReLU, PReLU, Conv2d, BatchNorm2d, PixelShuffle, Linear, Sigmoid
-from src.utils.loss_functions import mse_loss
+
 
 class ResidualBlock(torch.nn.Module):
     def __init__(self):
@@ -35,7 +35,7 @@ class Generator(torch.nn.Module):
         self.conv1 = Conv2d(1, 64, kernel_size=9, padding=4)
         self.prelu = PReLU()
 
-        self.layers = self.get_residual_blocks(num_blocks)
+        self.layers = self._get_residual_blocks(num_blocks)
 
         self.conv2 = Conv2d(64, 64, kernel_size=3, padding=1)
         self.bn2 = BatchNorm2d(64)
@@ -48,7 +48,7 @@ class Generator(torch.nn.Module):
 
         self.conv5 = Conv2d(64, 1, kernel_size=9, padding=4)
 
-    def get_residual_blocks(self, num_blocks):
+    def _get_residual_blocks(self, num_blocks):
         layers = []
         for i in range(num_blocks):
             layers.append(ResidualBlock())
@@ -92,7 +92,7 @@ class DiscriminatorBlock(torch.nn.Module):
 
 
 class Discriminator(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, high_res):
         super(Discriminator, self).__init__()
         print("Initialized discriminator network..")
         self.num_blocks = 3
@@ -107,7 +107,7 @@ class Discriminator(torch.nn.Module):
             DiscriminatorBlock(256, 512, 1, 1),
             DiscriminatorBlock(512, 512, 2, 1),
             Flatten(),
-            Linear(320000, 1024),
+            Linear(2*high_res*high_res, 1024),
             LeakyReLU(),
             Linear(1024, 1),
             Sigmoid()
@@ -120,33 +120,18 @@ class Discriminator(torch.nn.Module):
 ''' Super-Resolution GAN model class.'''
 
 
-class SuperResolutionGAN(torch.nn.Module):
-    # default parameters used in original paper
-    batch_size = None
-    num_generator_steps = None
-    num_discriminator_steps = None
-    num_epochs = 100000
-    generator = None
-    discriminator = None
-    learning_rate = 0.00001
+class SuperResolutionGAN:
 
-    def __init__(self):
+    def __init__(self, low_resolution_dim, high_resolution_dim, **kwargs):
         super(SuperResolutionGAN, self).__init__()
-        print("Initialized SR-GAN model..")
+        print("Initializing SR-GAN model.....")
+        self.lr_dim = low_resolution_dim
+        self.hr_dim = high_resolution_dim
+        self.generator = Generator()
+        self.discriminator = Discriminator(high_resolution_dim)
 
-    def forward(self, x):
-        generator = Generator()
-        sample_high_res = generator.forward(x)
-
-        discriminator = Discriminator()
-        pred = discriminator.forward(sample_high_res)
-        print("Discriminator predictions: {}".format(pred))
-
-    def test(self):
-        raise NotImplementedError
-
-    def run(self):
-        pass
+        for k, v in kwargs.items():
+            self.k = v
 
 
 class Flatten(torch.nn.Module):
--- src/utils/loss_functions.py
+++ src/utils/loss_functions.py
@@ -1,25 +1,77 @@
-'''
-Pixel-wise Mean Squared Error loss
-'''
+import torch
+from torch.nn import MSELoss, BCELoss
 
-def select_perceptive_loss(loss_name, params):
+
+def select_content_loss(loss_name, params):
     if loss_name is None:
         return None
-    elif loss_name=='l2':
-        raise NotImplementedError()
-    elif loss_name=='vgg':
-        raise NotImplementedError()
+    elif loss_name == 'l2':
+        return mse_loss(params)
+    elif loss_name == 'vgg':
+        return vgg_loss(params)
     else:
-        raise ValueError(f'Perceptive loss "{loss_name}" is not valid.')
+        raise ValueError(f'Content loss "{loss_name}" is not valid.')
 
 
-def mse_loss():
-    raise NotImplementedError()
+'''
+Pixel-wise Mean Squared Error loss.
+'''
+
+
+def mse_loss(params=None):
+    def _mse_loss(real_high_res, fake_high_res):
+        loss = torch.nn.MSELoss(real_high_res, fake_high_res)
+        return loss
+
+    return _mse_loss
+
+
+'''
+Pixel-wise Mean Squared Error based on VGG (trained) feature maps.
+'''
+
+
+def vgg_loss(params=None):
+    def _vgg_loss(real_high_res, fake_high_res, layer_num=2):
+        raise NotImplementedError()
+
+    return _vgg_loss
 
 
 def perceptual_loss():
     raise NotImplementedError()
 
 
-def adversarial_loss():
-    raise NotImplementedError()
+'''
+Takes in a batch of real and generated high resolution images, and returns a binary cross entropy loss.
+Input: 
+real_high_res: [batch_size, high_resolution_dim, high_resolution_dim], set of real high-res images.
+fake_high_res: [batch_size, high_resolution_dim, high_resolution_dim], set of fake high-res images.
+discriminator: Discriminator class object.
+
+Output:
+# loss: scalar, standard GAN loss.
+'''
+
+
+def gan_loss(params=None):
+    def _gan_loss(real_high_res, fake_high_res, discriminator):
+        num_real = len(real_high_res)
+        num_fake = len(fake_high_res)
+
+        batch_size = num_real + num_fake
+
+        x = torch.cat(real_high_res, fake_high_res)
+        y = torch.zeros(batch_size)
+        # set y = 1 for real data-set images
+        y[:num_real] = 1
+        y_predict = discriminator(x)
+
+        discriminator_loss = BCELoss()(y, y_predict)
+
+        # compute generator loss function only for fake images
+        generator_loss = -torch.log(y_predict[num_real:])
+
+        return discriminator_loss, generator_loss
+
+    return _gan_loss
