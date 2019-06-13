176a2abdfaf103a0645284c41f4e42b9130bb64d
Casey Graff <caseyagraff@gmail.com>
2019-06-12 17:09:50 -0700

    Updated model factory

--- src/models/model_factory.py
+++ src/models/model_factory.py
@@ -4,7 +4,12 @@ Instantiating models.
 
 from . import models
 
+class GAN:
+    def __init__(self, generator, discriminator):
+        self.generator = generator
+        self.discriminator = discriminator
+
 class ModelFactory:
     @staticmethod
     def create_model(model_name, model_params):
-        raise NotImplementedError()
+        return GAN(None, None)
